564f0e285c10786a040b96096799a311793384ab
tienduong <tienduong250196@gmail.com>
2017-12-12 10:54:44 +0700

    updated

---
+++ resources/views/layouts/users/main.blade.php
@@ -0,0 +1,8 @@
+@include("layouts.users.partisals.header")
+
+
+
+@yield('content');
+
+
+@include('layouts.users.partisals.footer');
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,45 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.users.main');
+});
+
+Route::get('/home',function(){
+	return view('layouts.users.home.index');
+});
+
+Route::get('/product',function(){
+	return view('layouts.users.product.product_detail');
+});
+
+Route::get('/cart',function(){
+	return view('layouts.users.cart.cart_detail');
+});
+
+Route::get('/customer/account',function(){
+	return view('layouts.users.customers.account');
+});
+
+Route::get('/customer/login',function(){
+	return view('layouts.users.customers.login');
+});
+
+Route::get('/customer/register',function(){
+	return view('layouts.users.customers.register');
+});
+
+Route::get('/about_us',function(){
+	return view('layouts.users.more_infomation.policy');
+});
+
+Route::get('/knowledge',function(){
+	return view('layouts.users.more_infomation.knowledge');
+});
+
+Route::get('/contact',function(){
+	return view('layouts.users.more_infomation.contact');
+});
+
+Route::get('/collections',function(){
+	return view('layouts.users.product.collections_products');
 });
