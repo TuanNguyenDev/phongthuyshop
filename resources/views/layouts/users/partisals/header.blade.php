f4d5afe199f99c0a68800e13e04001456da800b0
TuanLogan <anhtuanthpthn@gmail.com>
2017-12-13 12:28:44 +0700

    update

--- resources/views/layouts/users/partisals/header.blade.php
+++ resources/views/layouts/users/partisals/header.blade.php
@@ -183,11 +183,11 @@
 														<i class="sub-dropdown visible-sm visible-md visible-lg"></i> 
 														<div class="num-items-in-cart">
 															<div class="items-cart-left">
-																<img class="cart_img" src="./assets/images/bg-cart.png" alt="Image Cart" title="Image Cart">
+																<img class="cart_img" src="{{ asset('images/bg-cart.png') }}" alt="Image Cart" title="Image Cart">
 																<span class="cart_text icon"><span class="number">2</span></span>       
 															</div>
 															<div class="items-cart-right">
-																<span class="title_cart">My Cart</span>        
+																<span class="title_cart">Giỏ hàng</span>        
 															</div>
 														</div>
 													</a>
